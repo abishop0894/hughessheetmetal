b0239ef7e59ad15318b1762ebd5ff75e41acd96e
abishop0894 <abishop0894@gmail.com>
2025-06-16 13:49:10 -0400

    new

---
+++ app/careers/page.tsx
@@ -0,0 +1,12 @@
+import Page from "../modules/layout/Page"
+import HomeHero from "../modules/components/heros/HomeHero"
+
+const Careers = () => {
+  return (
+    <Page>
+    <HomeHero bottomBar="Careers" bottomBarBg="white" bottomBarText="black" arrowColor="black" backgroundImg={"https://images.unsplash.com/photo-1560448204-e02f11c3d0e2?ixlib=rb-4.0.3&auto=format&fit=crop&w=1920&h=1080&q=80"} />
+    </Page>
+  )
+}
+
+export default Careers
---
+++ app/portfolio/page.tsx
@@ -0,0 +1,14 @@
+import Page from "../modules/layout/Page"
+import HomeHero from "../modules/components/heros/HomeHero"
+import ContactForm from "../modules/components/contact/ContactForm"
+
+const Portfolio = () => {
+  return (
+    <Page>
+    <HomeHero bottomBar="Portfolio" bottomBarBg="white" bottomBarText="black" arrowColor="black" backgroundImg={"https://images.unsplash.com/photo-1560448204-e02f11c3d0e2?ixlib=rb-4.0.3&auto=format&fit=crop&w=1920&h=1080&q=80"} />
+    <ContactForm />
+    </Page>
+  )
+}
+
+export default Portfolio
